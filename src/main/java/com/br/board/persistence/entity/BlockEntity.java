506e09274c6d7de051a2f6d61e5947f38958fe80
Paulino <paulinomendes03@gmail.com>
2025-03-25 17:36:17 -0300

    feat: cria as entidades

---
+++ src/main/java/com/br/board/persistence/entity/BlockEntity.java
@@ -0,0 +1,14 @@
+package com.br.board.persistence.entity;
+
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class BlockEntity {
+    private Long id;
+    private OffsetDateTime blockedAt;
+    private String blockReason;
+    private OffsetDateTime unblockedAt;
+    private String unblockReason;
+}
