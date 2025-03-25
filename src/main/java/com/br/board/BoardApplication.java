d74000fa497b8808194f93524eb94628a78a432e
Paulino <paulinomendes03@gmail.com>
2025-03-25 14:56:21 -0300

    feat: inicio do projeto

---
+++ src/main/java/com/br/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package com.br.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		// Inicia o Spring Boot
+		SpringApplication.run(BoardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
