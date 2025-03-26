4b7deeb3a6a7ed3d149d885878605b592225f1e5
Paulino <paulinomendes03@gmail.com>
2025-03-26 16:35:46 -0300

    feat(BoardMenu): adiciona a opção de desbloquear card

--- src/main/java/com/br/board/service/CardService.java
+++ src/main/java/com/br/board/service/CardService.java
@@ -127,4 +127,26 @@ public class CardService {
             throw ex;
         }
     }
+
+    public void unblock(final Long id, final String reason) throws SQLException {
+        try{
+            var dao = new CardDAO(connection);
+            var optional = dao.findById(id);
+            var dto = optional.orElseThrow(
+                    () -> new EntityNotFoundException("O card de id %s não foi encontrado".formatted(id))
+            );
+
+            if (!dto.blocked()){
+                var message = "O card %s não está bloqueado".formatted(id);
+                throw new CardBlockedException(message);
+            }
+
+            var blockDAO = new BlockDAO(connection);
+            blockDAO.unblock(reason, id);
+            connection.commit();
+        }catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        }
+    }
 }
--- src/main/java/com/br/board/ui/BoardMenu.java
+++ src/main/java/com/br/board/ui/BoardMenu.java
@@ -104,6 +104,17 @@ public class BoardMenu {
     }
 
     private void unblockCard() throws SQLException {
+        System.out.println("Informe o id do card que será desbloqueado");
+        var cardId = scanner.nextLong();
+
+        System.out.println("Informe o motivo do desbloqueio do card");
+        var reason = scanner.next();
+
+        try(var connection = getConnection()){
+            new CardService(connection).unblock(cardId, reason);
+        } catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
     private void cancelCard() throws SQLException {
