0c92a5c11a943ac21f9e453d514e25ee41c9cc58
Paulino <paulinomendes03@gmail.com>
2025-03-26 00:53:55 -0300

    feat(MainMenu): cria o menu principal

---
+++ src/main/java/com/br/board/ui/BoardMenu.java
@@ -0,0 +1,12 @@
+package com.br.board.ui;
+
+import com.br.board.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BoardMenu {
+    private final BoardEntity entity;
+
+    public void execute() {
+    }
+}
